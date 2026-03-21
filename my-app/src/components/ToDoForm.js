39b574b2177e13d796965f8e5dc3fa81638d5aa4
heto66 <wi24b191@technikum-wien.at>
2026-03-21 12:33:34 +0100

    removed unnecessary imports

--- my-app/src/components/ToDoForm.js
+++ my-app/src/components/ToDoForm.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
+
 export const ToDoForm = () => {
   return (
-    <div>ToDoForm</div>
+    <form>ToDoForm</form>
   )
 }
--- my-app/src/components/ToDoWrapper.js
+++ my-app/src/components/ToDoWrapper.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export const ToDoWrapper = () => {
   return (
     <div>ToDoWrapper</div>
