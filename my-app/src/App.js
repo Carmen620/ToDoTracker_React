08644d9247c007e722629d37b4741e6761cf3174
heto66 <wi24b191@technikum-wien.at>
2026-03-21 12:29:39 +0100

    imported ToDoWrapper and css to App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,15 @@
+import './App.css';
+import { ToDoWrapper } from './components/ToDoWrapper';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1>My To-Do List Tracker</h1>
+      </header>
+      <main>
+        <ToDoWrapper />
+      </main>
     </div>
   );
 }
