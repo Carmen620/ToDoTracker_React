3ccb46c63348a3f536705c44c7f6b4fa74e23a68
heto66 <wi24b191@technikum-wien.at>
2026-03-21 13:58:08 +0100

    form handler works through printing to the console on web app

--- my-app/src/components/ToDoForm.js
+++ my-app/src/components/ToDoForm.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 
 export const ToDoForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const task = formData.get('task');
+        for(let [key, value] of formData.entries()) {
+        console.log(`${key}: ${value}`);
+        }
+    };
+
   return (
-    <form>
-        <input type="text" className='Form-input' placeholder='Enter To-Do'></input> 
+    <form onSubmit={handleSubmit}>
+        <input name='task' type="text" className='Form-input' placeholder='Enter To-Do'></input> 
         <button type="submit" className='Todo-btn'>Add To-Do</button>        
     </form>
   )
